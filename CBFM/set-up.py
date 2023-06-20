3f8c547449b5a2d97fe4b2e00e64d57c522f75c4
Spillias <spi085@csiro.au>
2023-06-20 10:26:52 +1000

    fix save

--- CBFM/set-up.py
+++ CBFM/set-up.py
@@ -5,6 +5,6 @@ topic = "'The barriers and benefits of Community-Based Fisheries Management in t
 ScreeningCriteria = [
    "Is it possible that the Title and Abstract summarise a paper with a case study from: Cook Islands, Federated States of Micronesia, Fiji, Kiribati, Marshall Islands, Nauru, Niue, Palau, Papua New Guinea, Samoa, Solomon Islands, Tonga, Tuvalu, Vanuatu",
    "Is it likely that the Title and Abstract summarise a paper that will discuss fisheries and/or marine resource management?",
-   "Is it likely that the Title and Abstract summarise a paper that will discuss a community-based approach? Exclude if there is a focus on co-management.",
-   "Is it likely that the Title and Abstract summarise a paper that is part of the peer reviewed published literature? Exclude grey literature, conference reports, and book chapters."
+  # "Is it likely that the Title and Abstract summarise a paper that will discuss a community-based approach? Exclude if there is a focus on co-management.",
+ #  "Is it likely that the Title and Abstract summarise a paper that is part of the peer reviewed published literature? Exclude grey literature, conference reports, and book chapters."
     ]
